fda67a8c1a28d25922d8fb9b1b836a11defc39fe
이상묵 <gogotr@kakao.com>
2021-04-21 01:50:45 +0900

    컴포넌트 최적화 하기

--- src/App.js
+++ src/App.js
@@ -3,21 +3,22 @@ import TodoInsert from './components/TodoInsert';
 import TodoList from './components/TodoList';
 import TodoTemplate from './components/TodoTemplate';
 
-const App = () => {
-  const [todos, setTodo] = useState([
-    {
-      id: 1,
-      text: 'hello world!',
-      checked: false,
-    },
-    {
-      id: 2,
-      text: 'hello world! 2',
-      checked: false,
-    },
-  ]);
+function createBulkTodos(){
+  const array = []
+  for(let i=1; i<=2000; i++){
+    array.push({
+      id:i,
+      text: `할일 ${i}`,
+      checked: false
+    })
+  }
+  return array
+}
 
-  const nextID = useRef(3);
+const App = () => {
+  const [todos, setTodo] = useState(createBulkTodos);
+
+  const nextID = useRef(2001);
 
   const onInsert = useCallback(
     (text) => {
@@ -26,29 +27,24 @@ const App = () => {
         text,
         checked: false,
       };
-      setTodo(todos.concat(todo));
+      setTodo(todos => todos.concat(todo));
       nextID.current += 1;
     },
-    [todos],
+    [],
   );
 
   const onToggle = useCallback(id =>{
-    setTodo(todos.map(todo => todo.id === id ? {...todo, checked:!todo.checked} : todo))
-  })
+    setTodo(todos => todos.map(todo => todo.id === id ? {...todo, checked:!todo.checked} : todo))
+  },[])
 
   const onRemove = useCallback(id =>{
-    setTodo(todos.filter(todo => todo.id !== id))
-  },[todos])
+    setTodo(todos => todos.filter(todo => todo.id !== id))
+  },[])
 
   return (
       <TodoTemplate>
-<<<<<<< HEAD
         <TodoInsert onInsert={onInsert} />
         <TodoList todos={todos} onRemove={onRemove} onToggle={onToggle}></TodoList>
-=======
-        <TodoInsert></TodoInsert>
-        
->>>>>>> 912166a488e2ee3c9fc3d32dbccf6a25fc35b443
       </TodoTemplate>
   );
 };
